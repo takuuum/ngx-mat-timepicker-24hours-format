bbf9295dcdb24dae367574a1f584d0008e14d1fe
takuuum <58619805+takuuum@users.noreply.github.com>
2023-06-01 17:54:52 +0900

    delete: unuse codes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,32 +8,4 @@ import { DateTime } from 'ts-luxon';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  formControlItem: FormControl = new FormControl('');
-  maxTime: DateTime = DateTime.local().set({
-    hour: 16,
-  });
-  minTime: DateTime = DateTime.local().set({
-    hour: 14,
-  });
-  required: boolean = !1;
-
-  @ViewChild('timepicker') timepicker: any;
-
-  /**
-   * Lets the user click on the icon in the input.
-   */
-  openFromIcon(timepicker: { open: () => void }) {
-    if (!this.formControlItem.disabled) {
-      timepicker.open();
-    }
-  }
-
-  /**
-   * Function to clear FormControl's value, called from the HTML template using the clear button
-   *
-   * @param $event - The Event's data object
-   */
-  onClear($event: Event) {
-    this.formControlItem.setValue(null);
-  }
 }
